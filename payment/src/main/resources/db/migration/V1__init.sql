c83b19a7e9cd8025b81dcf6be90653a453021313
mikhail.goncharenko <naatsms@gmail.com>
2024-03-25 00:00:42 +0100

    Enables Flyway, adds sample migration script

---
+++ payment/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Merchant (
+                          MerchantId SERIAL PRIMARY KEY,
+                          MerchantName VARCHAR(255),
+                          SecretKey VARCHAR(255)
+);
