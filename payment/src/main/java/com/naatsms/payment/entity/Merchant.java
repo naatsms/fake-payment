661a8702326cee1a1e31da2c01f2fa6b1966a39d
mikhail.goncharenko <naatsms@gmail.com>
2024-03-29 13:22:48 +0100

    Reworks merchant entity to record

--- payment/src/main/java/com/naatsms/payment/entity/Merchant.java
+++ payment/src/main/java/com/naatsms/payment/entity/Merchant.java
@@ -5,42 +5,8 @@ import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Table("merchant")
-public class Merchant{
-
-    @Id
-    @Column("merchantId")
-    Long merchantId;
-    @Column("merchantName") String name;
-    @Column("secretKey") String secretKey;
-
-    public Long getMerchantId()
-    {
-        return merchantId;
-    }
-
-    public void setMerchantId(final Long id)
-    {
-        this.merchantId = id;
-    }
-
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(final String name)
-    {
-        this.name = name;
-    }
-
-    public String getSecretKey()
-    {
-        return secretKey;
-    }
-
-    public void setSecretKey(final String secretKey)
-    {
-        this.secretKey = secretKey;
-    }
-}
-
+public record Merchant(
+        @Id Long id,
+        @Column String name,
+        @Column String secret
+){}
