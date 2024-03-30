73f4e0b4614077e3a392fd1961297e89780847c5
mikhail.goncharenko <naatsms@gmail.com>
2024-03-30 16:43:10 +0100

    Custom authentication filter

--- payment/src/main/java/com/naatsms/payment/config/SecurityConfig.java
+++ payment/src/main/java/com/naatsms/payment/config/SecurityConfig.java
@@ -1,17 +1,15 @@
 package com.naatsms.payment.config;
 
-import com.naatsms.payment.repository.MerchantRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
-import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.WebFilter;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -24,16 +22,10 @@ public class SecurityConfig
     }
 
     @Bean
-    ReactiveAuthenticationManager authenticationManager(ReactiveUserDetailsService userDetailsService) {
-        return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
-    }
-
-    @Bean
-    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http, ReactiveAuthenticationManager authenticationManager) {
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(authorize -> authorize.anyExchange().authenticated())
-            .httpBasic(basic -> basic.authenticationManager(authenticationManager));
-        return http.build();
+    SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http, @Qualifier("basicAuthWebFilter") WebFilter authFilter) {
+        return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .addFilterAt(authFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+            .build();
     }
 
 }
---
+++ payment/src/main/java/com/naatsms/payment/security/BasicAuthWebFilter.java
@@ -0,0 +1,64 @@
+package com.naatsms.payment.security;
+
+import com.naatsms.payment.repository.MerchantRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Component
+@Qualifier("basicAuthWebFilter")
+public class BasicAuthWebFilter implements WebFilter
+{
+    public static final String BASIC = "Basic ";
+    public static final String DELIMITER = ":";
+    private final MerchantRepository merchantRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public BasicAuthWebFilter(final MerchantRepository merchantRepository, final PasswordEncoder passwordEncoder)
+    {
+        this.merchantRepository = merchantRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain)
+    {
+        String[] credentials = getCredentials(exchange);
+        return merchantRepository.findByName(credentials[0])
+                .filter(merchant -> passwordEncoder.matches(credentials[1], merchant.secret()))
+                .switchIfEmpty(Mono.error(new BadCredentialsException("Bad credentials")))
+                .doOnNext(merchant -> exchange.getAttributes().put("merchant", merchant.id()))
+                .flatMap(f -> chain.filter(exchange));
+    }
+
+    private String[] getCredentials(final ServerWebExchange exchange)
+    {
+        String header = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (header == null) {
+            throw new BadCredentialsException("No authentication provided");
+        }
+        header = header.trim();
+        if (!StringUtils.startsWithIgnoreCase(header, BASIC)) {
+            throw new BadCredentialsException("Invalid authentication format");
+        }
+        byte[] base64Token = header.substring(6).getBytes(StandardCharsets.UTF_8);
+        byte[] decoded = Base64.getDecoder().decode(base64Token);
+        String token = new String(decoded);
+        String[] credentials = token.split(DELIMITER);
+        if (credentials.length != 2) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return credentials;
+    }
+
+}
