3db4e8e727772db863bd529a79341ccbcb66d53b
mikhail.goncharenko <naatsms@gmail.com>
2024-04-09 21:52:40 +0200

    Support for payouts

--- payment/src/main/java/com/naatsms/payment/controller/TransactionController.java
+++ payment/src/main/java/com/naatsms/payment/controller/TransactionController.java
@@ -3,19 +3,18 @@ package com.naatsms.payment.controller;
 import com.naatsms.payment.dto.PaymentTransactionDto;
 import com.naatsms.payment.dto.TransactionResponseDto;
 import com.naatsms.payment.entity.PaymentTransaction;
+import com.naatsms.payment.enums.TransactionType;
 import com.naatsms.payment.service.TransactionService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import static com.naatsms.payment.constants.SessionKeys.MERCHANT_ID;
 
 @RestController
+@RequestMapping("/api/v1/payments")
 public class TransactionController
 {
     private static final Logger LOG = LoggerFactory.getLogger(TransactionController.class);
@@ -27,16 +26,23 @@ public class TransactionController
         this.transactionService = transactionService;
     }
 
-    @PostMapping("/transaction")
-    public Mono<TransactionResponseDto> createTransaction(@RequestBody PaymentTransactionDto transaction, ServerWebExchange exchange) {
+    @PostMapping("/topups/")
+    public Mono<TransactionResponseDto> createTopUp(@RequestBody PaymentTransactionDto transaction, ServerWebExchange exchange) {
         LOG.info("{} is being created...", transaction);
-        return transactionService.createTransaction(transaction, exchange.getAttribute(MERCHANT_ID))
+        return transactionService.createTransaction(transaction, TransactionType.TRANSACTION, exchange.getAttribute(MERCHANT_ID))
+                .map(this::toDto);
+    }
+
+    @PostMapping("/payouts/")
+    public Mono<TransactionResponseDto> createPayout(@RequestBody PaymentTransactionDto transaction, ServerWebExchange exchange) {
+        LOG.info("{} is being created...", transaction);
+        return transactionService.createTransaction(transaction, TransactionType.PAYOUT, exchange.getAttribute(MERCHANT_ID))
                 .map(this::toDto);
     }
 
     private TransactionResponseDto toDto(final PaymentTransaction paymentTransaction)
     {
-        return new TransactionResponseDto(paymentTransaction.uuid().toString(), paymentTransaction.status(), "OK");
+        return new TransactionResponseDto(paymentTransaction.getUuid().toString(), paymentTransaction.getStatus(), "OK");
     }
 
     @ExceptionHandler
--- payment/src/main/java/com/naatsms/payment/service/TransactionService.java
+++ payment/src/main/java/com/naatsms/payment/service/TransactionService.java
@@ -2,9 +2,10 @@ package com.naatsms.payment.service;
 
 import com.naatsms.payment.dto.PaymentTransactionDto;
 import com.naatsms.payment.entity.PaymentTransaction;
+import com.naatsms.payment.enums.TransactionType;
 import reactor.core.publisher.Mono;
 
 public interface TransactionService
 {
-    Mono<PaymentTransaction> createTransaction(PaymentTransactionDto transactionData, final Long merchant);
+    Mono<PaymentTransaction> createTransaction(PaymentTransactionDto transactionData, TransactionType type, final Long merchant);
 }
