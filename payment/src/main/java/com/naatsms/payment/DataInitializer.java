00ec9778ddd37ee20a4c29edb569d2846599ccaf
mikhail.goncharenko <naatsms@gmail.com>
2024-04-09 21:53:23 +0200

    Improves data initializer

--- payment/src/main/java/com/naatsms/payment/DataInitializer.java
+++ payment/src/main/java/com/naatsms/payment/DataInitializer.java
@@ -1,12 +1,17 @@
 package com.naatsms.payment;
 
+import com.naatsms.payment.entity.AccountBalance;
 import com.naatsms.payment.entity.Merchant;
+import com.naatsms.payment.repository.AccountRepository;
 import com.naatsms.payment.repository.MerchantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
 
 @Component
 public class DataInitializer implements ApplicationRunner
@@ -14,19 +19,29 @@ public class DataInitializer implements ApplicationRunner
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
     @Autowired
     private MerchantRepository merchantRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
     @Override
     public void run(final ApplicationArguments args)
     {
-        try
-        {
-            Merchant merchant = new Merchant(null, "name", passwordEncoder.encode("secret"));
-            merchantRepository.save(merchant).subscribe();
-        } catch (Exception e) {
-            // ignore
-        }
+            merchantRepository.findByName("name")
+                    .switchIfEmpty(getMerchant())
+                    .flatMap(merchant -> accountRepository.findByMerchantIdAndCurrencyIso(merchant.id(), "BRL")
+                            .switchIfEmpty(accountRepository.save(getAccount(merchant))))
+                    .subscribe();
+    }
+
+    private AccountBalance getAccount(final Merchant merchant)
+    {
+        return new AccountBalance(null, merchant.id(), "BRL", BigDecimal.valueOf(1000));
+    }
+
+    private Mono<Merchant> getMerchant()
+    {
+        Merchant merchant = new Merchant(null, "name", passwordEncoder.encode("secret"));
+        return merchantRepository.save(merchant);
     }
 }
