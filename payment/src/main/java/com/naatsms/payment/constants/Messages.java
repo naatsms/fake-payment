f40dc642a4bde3e1d2b87f47b447ce2261801834
mikhail.goncharenko <naatsms@gmail.com>
2024-04-13 00:06:22 +0200

    Error messages

--- payment/src/main/java/com/naatsms/payment/constants/Messages.java
+++ payment/src/main/java/com/naatsms/payment/constants/Messages.java
@@ -2,8 +2,11 @@ package com.naatsms.payment.constants;
 
 public final class Messages
 {
+
     private Messages() {}
 
+    public static final String ERROR = "ERROR";
+    public static final String FAILED = "FAILED";
     public static final String OK = "OK";
 
 }
