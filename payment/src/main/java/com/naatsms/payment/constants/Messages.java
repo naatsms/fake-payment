f129b5fd1f644cdb0bc640188d4fef7a1ca7444e
mikhail.goncharenko <naatsms@gmail.com>
2024-04-16 22:46:24 +0200

    Error messages

--- payment/src/main/java/com/naatsms/payment/constants/Messages.java
+++ payment/src/main/java/com/naatsms/payment/constants/Messages.java
@@ -3,10 +3,12 @@ package com.naatsms.payment.constants;
 public final class Messages
 {
 
-    private Messages() {}
-
+    public static final String PAYMENT_METHOD_NOT_ALLOWED = "PAYMENT_METHOD_NOT_ALLOWED";
+    public static final String PAYOUT_MIN_AMOUNT = "PAYOUT_MIN_AMOUNT";
     public static final String ERROR = "ERROR";
     public static final String FAILED = "FAILED";
     public static final String OK = "OK";
+
+    private Messages() {}
 
 }
