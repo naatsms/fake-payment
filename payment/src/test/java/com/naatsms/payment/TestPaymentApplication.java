d9d9a13c994166e8f58287b7d7f804f9add61ee1
mikhail.goncharenko <naatsms@gmail.com>
2024-04-17 01:13:48 +0200

    NoOpNotificationService implementation for tests

--- payment/src/test/java/com/naatsms/payment/TestPaymentApplication.java
+++ payment/src/test/java/com/naatsms/payment/TestPaymentApplication.java
@@ -1,14 +1,23 @@
 package com.naatsms.payment;
 
+import com.naatsms.payment.service.NotificationService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
+import reactor.core.publisher.Mono;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestPaymentApplication {
+
+	@Bean
+	@Profile("test")
+	NotificationService notificationService() {
+		return transaction -> Mono.empty();
+	}
 
 	@Bean
 	@ServiceConnection
