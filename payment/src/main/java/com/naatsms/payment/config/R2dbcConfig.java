96c6d868aae12771fa0319103dc5f8ac727c7d42
mikhail.goncharenko <naatsms@gmail.com>
2024-04-17 00:59:35 +0200

    cleanup r2dbc config

--- payment/src/main/java/com/naatsms/payment/config/R2dbcConfig.java
+++ payment/src/main/java/com/naatsms/payment/config/R2dbcConfig.java
@@ -1,11 +1,8 @@
 package com.naatsms.payment.config;
 
-import io.r2dbc.spi.ConnectionFactories;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcConnectionDetails;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.reactive.TransactionalOperator;
@@ -13,13 +10,7 @@ import org.springframework.transaction.reactive.TransactionalOperator;
 @Configuration
 @EnableR2dbcRepositories
 @EnableTransactionManagement
-public class R2dbcConfig
-{
-
-    @Bean
-    ReactiveTransactionManager reactiveTransactionManager(R2dbcConnectionDetails details) {
-        return new R2dbcTransactionManager(ConnectionFactories.get(details.getConnectionFactoryOptions()));
-    }
+public class R2dbcConfig {
 
     @Bean
     TransactionalOperator transactionalOperator(ReactiveTransactionManager transactionManager) {
