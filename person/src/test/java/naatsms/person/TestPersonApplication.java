1290de3d25a82ba1f17311d48274c1ec59089e99
mikhail.goncharenko <naatsms@gmail.com>
2024-04-21 16:23:45 +0200

    Person API initial commit

---
+++ person/src/test/java/naatsms/person/TestPersonApplication.java
@@ -0,0 +1,23 @@
+package naatsms.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestPersonApplication {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.from(PersonApplication::main).with(TestPersonApplication.class).run(args);
+    }
+
+}
