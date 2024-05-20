808a703435b10f32201d8c7919e05444e41448f5
Mikhail Goncharenko <naatsms@gmail.com>
2024-05-20 17:11:39 +0200

    [Person] Disables default security

--- person/src/main/java/naatsms/person/PersonApplication.java
+++ person/src/main/java/naatsms/person/PersonApplication.java
@@ -2,8 +2,9 @@ package naatsms.person;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ReactiveSecurityAutoConfiguration.class})
 public class PersonApplication {
 
     public static void main(String[] args) {
