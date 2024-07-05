118a50d3b83839674c6628a5c84bbc8bd9be3e05
Mikhail Goncharenko <naatsms@gmail.com>
2024-07-05 16:55:15 +0200

    [Orchestra] enable webflux

--- orchestra/src/main/java/naatsms/orchestra/OrchestraApplication.java
+++ orchestra/src/main/java/naatsms/orchestra/OrchestraApplication.java
@@ -2,8 +2,10 @@ package naatsms.orchestra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableWebFlux
 public class OrchestraApplication {
 
 	public static void main(String[] args) {
