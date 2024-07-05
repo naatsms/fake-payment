c5cea86210a7d71710905a6287092662b12ad15e
Mikhail Goncharenko <naatsms@gmail.com>
2024-07-05 17:31:26 +0200

    [Orchestra] Constants

---
+++ orchestra/src/main/java/naatsms/orchestra/constants/Constants.java
@@ -0,0 +1,11 @@
+package naatsms.orchestra.constants;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String CLAIM_UUID = "uuid";
+    public static final String REALM_NAME = "payment";
+
+}
